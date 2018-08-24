ff91392a6f869c36183b7a0adcca2eab14680e3f
Miky9(home) <mikyf9@windowslive.com>
2018-08-24 23:30:05 +0200

    1.1.0

---
+++ Day_06_Review/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day_06_Review
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string S = Console.In.ReadLine();
+                int max = S.Length;
+                string part1 = "";
+                string part2 = "";
+                for (int j = 0; j < max; j++)
+                {
+                    if (j % 2 == 0)
+                        part1 += S[j];
+                    else
+                        part2 += S[j];
+                }
+               result = result + (part1 + " " + part2 + "\n");
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
