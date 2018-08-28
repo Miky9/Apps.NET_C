ab6b8635f476aeb05c9c7d8098e4d294221da00d
Miky9(home) <mikyf9@windowslive.com>
2018-08-28 23:54:49 +0200

    1.3.0

---
+++ C_1_0_HackerRank_30/Day_07_Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day_07_Arrays
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Array.Reverse(arr);
+
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            //Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_1_0_HackerRank_30/Day_08_Dictionaries_and_Maps/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_08_Dictionaries_and_Maps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int j = 1; j <= n; j++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                string name = tokens[0];
+                int phone = int.Parse(tokens[1]);
+                dict.Add(name, phone);
+            }
+
+            foreach (KeyValuePair<string, int> item in dict)
+            {
+                if (item.Key != null)
+                {
+                    Console.WriteLine("{0}={1}", item.Key, item.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
