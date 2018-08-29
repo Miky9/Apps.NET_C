fbd0e6e647575372cf23c78e7e4fd316c6fa8b16
Miky9(home) <mikyf9@windowslive.com>
2018-08-29 03:40:55 +0200

    1.4.0

--- C_1_0_HackerRank_30/Day_08_Dictionaries_and_Maps/Program.cs
+++ C_1_0_HackerRank_30/Day_08_Dictionaries_and_Maps/Program.cs
@@ -1,37 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Day_08_Dictionaries_and_Maps
 {
-    class Program
+    class Solution
     {
         static void Main(string[] args)
         {
+            //creating dictionary with n items
             int n = Convert.ToInt32(Console.ReadLine());
 
             IDictionary<string, int> dict = new Dictionary<string, int>();
 
             for (int j = 1; j <= n; j++)
             {
-                string[] tokens = Console.ReadLine().Split(' ');
+                string line = Console.ReadLine();
+                line.Trim();
+                line += " ";
+                
+                string[] tokens = line.Split(' ');
                 string name = tokens[0];
-                int phone = int.Parse(tokens[1]);
-                dict.Add(name, phone);
-            }
-
-            foreach (KeyValuePair<string, int> item in dict)
-            {
-                if (item.Key != null)
+                if (tokens[1] != "")
                 {
-                    Console.WriteLine("{0}={1}", item.Key, item.Value);
+                    int phone = int.Parse(tokens[1]);
+                    dict.Add(name, phone);
                 }
                 else
                 {
+                    int phone = 0;
+                    dict.Add(name, phone);
+                }
+                
+            }
+
+            //x inputs
+            List<string> temp_list = new List<string>();
+            string temp_line = "";
+
+            while (true)
+            {
+                    temp_line = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(temp_line))
+                        temp_list.Add(temp_line);
+                    else
+                        break;
+            }
+
+            //output
+            foreach (string item in temp_list)
+            {
+                try
+                {
+                    Console.WriteLine("{0}={1}", item, dict[item]);
+                }
+                catch (Exception ex)
+                {
                     Console.WriteLine("Not found");
+                    //throw;
                 }
             }
+
 
             Console.ReadKey();
         }
     }
+    
 }
